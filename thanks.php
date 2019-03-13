e36a1165fe6e8e073acf1e6774caf2f7baf70589
Atsushi <atsushi88885@gmail.com>
2019-03-13 22:59:05 +0900

    PDO修正

--- thanks.php
+++ thanks.php
@@ -1,24 +1,33 @@
 <?php
-    //check.phpから送られてきた内容を変数に代入
-    $nickname = htmlspecialchars($_POST['nickname']);
-    $email = htmlspecialchars($_POST['email']);
-    $content = htmlspecialchars($_POST['content']);
+    require_once('function.php');
+
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $content = $_POST['content'];
 
     //DBに接続
-    $dsn = 'mysql:dbname=phpkiso;host=localhost';
+    $host = "localhost";
+    $dbname = "phpkiso";
+    $charset = "utf8mb4";
     $user = 'root';
     $password='';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->query('SET NAMES utf8');
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
 
-    //SQLを実行する
-    //SQLインジェクション対策(プリペアドステートメント)
-    $sql = 'INSERT INTO survey (nickname, email, content) VALUES (?, ?, ?)';
-    $data = [$nickname, $email, $content];//?を置きかえる用の変数($data)を作成
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute($data);//?を$dataに置き換えてSQLを実行
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+    try {
+        $dbh = new PDO($dsn, $user, $password, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+    
+    //SQLを実行
+    $stmt = $dbh->prepare('INSERT INTO survey (nickname, email, content) VALUES (?, ?, ?)');
+    $stmt->execute([$nickname, $email, $content]);//?を変数に置き換えてSQLを実行
 
-    //DBを切断
     $dbh = null;
 
 ?>
@@ -30,8 +39,8 @@
 </head>
 <body>
   <h1>お問い合わせありがとうございました！</h1>
-  <p><?php echo $nickname; ?></p>
-  <p><?php echo $email; ?></p>
-  <p><?php echo $content; ?></p>
+  <p><?php echo h($nickname); ?></p>
+  <p><?php echo h($email); ?></p>
+  <p><?php echo h($content); ?></p>
 </body>
 </html>
