3cb61516d073aa88edc362a748858fab689701da
Atsushi <atsushi88885@gmail.com>
2019-03-15 11:14:07 +0900

    インデント修正

--- thanks.php
+++ thanks.php
@@ -1,7 +1,7 @@
 <?php
 
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-      header('Location: index.html');
+        header('Location: index.html');
     }
 
     require_once('function.php');
@@ -18,13 +18,13 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <title>送信完了</title>
-  <meta charset="utf-8">
+    <title>送信完了</title>
+    <meta charset="utf-8">
 </head>
 <body>
-  <h1>お問い合わせありがとうございました！</h1>
-  <p><?php echo h($nickname); ?></p>
-  <p><?php echo h($email); ?></p>
-  <p><?php echo h($content); ?></p>
+    <h1>お問い合わせありがとうございました！</h1>
+    <p><?php echo h($nickname); ?></p>
+    <p><?php echo h($email); ?></p>
+    <p><?php echo h($content); ?></p>
 </body>
 </html>
